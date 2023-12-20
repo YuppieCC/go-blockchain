4d835a72680fc5c936d8e2899b234a6a8c3daa1e
eacentcheung <eacentcheung@gmail.com>
2023-12-20 14:55:21 +0700

    🛠️ init project

---
+++ blockchain.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+)
+
+type Block struct {
+	data map[string]interface {}
+	hash string
+	previousHash string
+	timestamp time.Time
+	pow int
+}
+
+type Blockchain struct {
+	genesisBlock Block
+	chain []Block
+	difficulty int
+}
+
+func main() {
+	// todo
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module demo/blockchain
+
+go 1.21.5
